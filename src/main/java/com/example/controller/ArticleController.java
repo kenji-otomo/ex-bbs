d78f5e5a86a1e0149dde8cd7311dcf754995153f
kenji-otomo <kenji.otomo@rakus-partners.co.jp>
2021-11-09 15:21:51 +0900

    Articleコントローラーのメソッド完成

--- src/main/java/com/example/controller/ArticleController.java
+++ src/main/java/com/example/controller/ArticleController.java
@@ -1,11 +1,13 @@
 package com.example.controller;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.domain.Article;
 import com.example.form.ArticleForm;
 import com.example.repository.ArticleRepository;
 
@@ -27,5 +29,23 @@ public class ArticleController {
 		model.addAttribute("artList",repository.findAll());
 		
 		return "board";
+	}
+	
+	@RequestMapping("/insert")
+	public String insertArticle(ArticleForm form,Model model) {
+		Article article = new Article();
+		BeanUtils.copyProperties(form, article);
+		
+		repository.insert(article);
+		
+		return index(model);
+	}
+	
+	@RequestMapping("/delete")
+	public String deleteArticle(Integer id,Model model) {
+		
+		repository.deleteById(id);
+		
+		return index(model);
 	}
 }
